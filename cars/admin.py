2ce37d6688f074f981f136e29d7801c45a380c32
unknown <rubencavalcanti@gmail.com>
2024-02-03 23:32:35 -0300

    criação dos models views urls e templates

--- cars/admin.py
+++ cars/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
+
 
 # Register your models here.
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model','brand','factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    search_fields = ('model', 'brand')
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
